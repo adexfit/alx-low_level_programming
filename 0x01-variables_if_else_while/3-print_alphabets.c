d3424cb346d2aec8a5f871af2827dcc447994be4
adexfit <y.adeboye@gmail.com>
2022-10-16 11:32:41 -0700

    added a new script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+* main - prints the alphabet in lowercase, and then in uppercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int alph;
+
+	for (alph = 'a'; ch <= 'z'; alph++)
+		putchar(alph);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
+
+}
